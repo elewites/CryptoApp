12b479015897366f77b85bf49a15d9d9948d23db
Eros Rodriguez <elewites@gmail.com>
2021-12-27 14:40:14 -0800

    finished coinpage

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,25 +2,25 @@
 import React, { useContext } from "react";
 import Switch from "@mui/material/Switch";
 import FormControlLabel from "@mui/material/FormControlLabel";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 //components
 import { Context } from "../Helpers/Context";
 import "./styling/navbar.css";
+import backArrow from "../back_arrow.png";
+import backArrowTwo from "../back_arrow_two.png";
 
 function Navbar() {
   //searchTerm global state
-  const {
-    setSearchTerm,
-    currency,
-    setCurrency,
-    setIsDarkMode,
-    isDarkMode,
-  } = useContext(Context);
+  const { setSearchTerm, currency, setCurrency, setIsDarkMode, isDarkMode } =
+    useContext(Context);
 
   //parameters stored in location route
   //used to determine if search bar renders
   const { id } = useParams();
+
+  //used to navigate to other routes
+  const navigate = useNavigate();
 
   //MODIFIES: searchTerm
   //EFFECTS: sets the searchTerm to current value of input
@@ -69,6 +69,21 @@ function Navbar() {
           control={<Switch onChange={toggleDarkMode} />}
         />
       </div>
+      {id && (
+        <div className="button-container">
+          <button
+            className={
+              isDarkMode ? "back-button back-button-dark" : "back-button"
+            }
+            onClick={() => {
+              console.log("hi");
+              navigate(-1);
+            }}
+          >
+            Return
+          </button>
+        </div>
+      )}
     </div>
   );
 }
